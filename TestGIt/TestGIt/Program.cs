e3f4456e9648ec4f33d94e6f0047622c81daf3d2
Miha Gorup <mihagorup123@gmail.com>
2022-10-13 10:21:50 +0200

    Klici in classi

--- TestGIt/TestGIt/Program.cs
+++ TestGIt/TestGIt/Program.cs
@@ -10,6 +10,15 @@ namespace TestGIt
     {
         static void Main(string[] args)
         {
+            Kupec Janez = new Kupec();
+            Janez.Ime = "janez Novak";
+            Janez.beležiKlic(10, TipKlica.Stacionarno);
+            Console.WriteLine(Janez.Ime+" dolguje "+Janez.Stanje);
+
+            Kupec Alenka = new Kupec();
+            Alenka.Ime = "Alenka Novak";
+            Alenka.beležiKlic(10, TipKlica.Mobilno);
+            Console.WriteLine(Alenka.Ime + " dolguje " + Alenka.Stanje);
 
             Console.ReadLine();
         }
---
+++ TestGIt/TestGIt/Properties/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGIt
+{
+    internal class Kupec
+    {
+        private string ime;
+        private decimal stanje;
+
+        public decimal Stanje { get => stanje;}
+        public string Ime { get => ime; set => ime = value; }
+
+        public void BeležiPlačilo(decimal plačilo)
+        {
+            stanje -= plačilo;
+        }
+
+        public void beležiKlic(int min,TipKlica tip)
+        {
+            switch(tip)
+            {
+                case TipKlica.Stacionarno:
+                    stanje+=min*0.12m;
+                    break;
+
+                case TipKlica.Mobilno:
+                    stanje += min * 0.03m;
+                    break;
+                default:
+                    break;
+
+                
+            }
+        }
+    }
+}
